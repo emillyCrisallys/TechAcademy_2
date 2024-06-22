db3f1119ab9af375f0f03b6c8e2e1431952c1f67
emillyCrisallys <crisallisnunes83@gmail.com>
2024-06-22 19:46:35 -0300

    Atualizar

--- menu.js
+++ menu.js
@@ -1,13 +1,13 @@
 
 var menuItem = document.querySelectorAll('.item-menu')
 
-function selectlink(){
-    menuItem.forEach((item)=>
-    item.classList.remove('ativo'))
+function selectlink() {
+    menuItem.forEach((item) =>
+        item.classList.remove('ativo'))
     this.classList.add('ativo')
 }
 
-menuItem.forEach((item)=>
+menuItem.forEach((item) =>
     item.addEventListener('click', selectlink)
 )
 
@@ -15,7 +15,7 @@ menuItem.forEach((item)=>
 var btnExp = document.querySelector('#btn-exp')
 var menusite = document.querySelector('.menu-lateral')
 
-btnExp.addEventListener('click', function(){
+btnExp.addEventListener('click', function () {
     menusite.classList.toggle('expandir')//toggle verifica se a função de expandir está ativa ou não
 
 })
